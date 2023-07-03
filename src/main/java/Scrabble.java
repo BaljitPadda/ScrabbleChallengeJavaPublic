5e4a8776a5efe6ebc559d032e31bfe0c7860820d
baljitpadda <baljitpadda@hotmail.co.uk>
2023-07-03 20:51:33 +0100

    First test passed, return 0 for an empty string. Constructor and score method created.

--- src/main/java/Scrabble.java
+++ src/main/java/Scrabble.java
@@ -1,2 +1,13 @@
 public class Scrabble {
+    private String word;
+    public Scrabble(String word) {
+        this.word = word;
+    }
+
+    public int score() {
+        if(this.word.equals(""))
+            return 0;
+        return 0;
+    }
+
 }
